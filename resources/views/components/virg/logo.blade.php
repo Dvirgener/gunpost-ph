bd02b3d3c722e4b92a4abafd83786f54f7e8d4b2
Dvirgener <142855933+Dvirgener@users.noreply.github.com>
2026-02-21 11:01:29 +0800

    fixed sidebard navigation for guests and logo with light/dark modes

---
+++ resources/views/components/virg/logo.blade.php
@@ -0,0 +1,7 @@
+<div>
+
+    <img src="{{ asset('gunpost-logos/gun_post_ph_light.png') }}" alt="Gun Post PH Logo" class="w-full dark:hidden">
+</div>
+<div>
+    <img src="{{ asset('gunpost-logos/gun_post_ph_dark.png') }}" alt="Gun Post PH Logo" class="w-full hidden dark:block">
+</div>
